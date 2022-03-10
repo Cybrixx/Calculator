4b9b36e6e84510025aa6220aed898d45c903f2ee
Cybrixx <49454838+Cybrixx@users.noreply.github.com>
2022-03-10 09:16:45 +0100

    Create Calculator.java
    Added Src-Folder
    Added Java-Class 'src.Calcualator'

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static int sum(int... numbers){
+        int sum = 0;
+        for(int number : numbers){
+            sum += number;
+        }
+        return sum;
+    }
+}
